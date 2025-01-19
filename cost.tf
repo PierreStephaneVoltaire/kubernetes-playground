d2a91394c8dfbf2273b149272b208b9d9ed574cf
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2025-01-18 20:21:08 -0500

    added budget for stack

--- cost.tf
+++ cost.tf
@@ -14,7 +14,7 @@ resource "aws_budgets_budget" "monthly_budget" {
 
   notification {
     comparison_operator        = "GREATER_THAN"
-    threshold                  = 80 
+    threshold                  = 80
     threshold_type             = "PERCENTAGE"
     notification_type          = "FORECASTED"
     subscriber_email_addresses = [var.contact]
