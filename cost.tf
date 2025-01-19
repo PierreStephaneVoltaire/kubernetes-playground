646b7608610e6fe24eddd04e4af8f033246125a3
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2025-01-18 20:19:53 -0500

    added cost report

---
+++ cost.tf
@@ -0,0 +1,23 @@
+resource "aws_budgets_budget" "monthly_budget" {
+  name         = "Monthly${var.app_name}Budget"
+  budget_type  = "COST"
+  limit_amount = "200"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  cost_filter {
+    name = "TagKeyValue"
+    values = ["Project${"$"}${var.tags.Project}"
+
+    ]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80 
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = [var.contact]
+
+  }
+}
