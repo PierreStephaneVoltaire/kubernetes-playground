a63c06f3f69fa3c09d8e26b128808ad62d79a81a
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2025-01-25 17:03:26 -0500

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -37,3 +37,12 @@ module "eks" {
   eks_managed_node_groups = var.eks_managed_node_groups
 }
 
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
