61898676c27483181ef3d7fa14c9abda6bbee609
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2025-02-05 19:29:33 -0500

    updated cost tag

--- cost/cost.tf
+++ cost/cost.tf
@@ -4,40 +4,13 @@ resource "aws_budgets_budget" "monthly_budget" {
   limit_amount = "200"
   limit_unit   = "USD"
   time_unit    = "MONTHLY"
-
   cost_filter {
     name = "TagKeyValue"
-    values = ["Project${"$"}${var.tags.Project}"
+    values = ["user:Project${"$"}${var.tags.Project}"
 
     ]
   }
-  cost_filter {
-    name = "Service"
-    values = [
-      "Amazon EC2 Container Registry (ECR)",
-      "Amazon Elastic Load Balancing",
-      "Amazon Elastic Container Service for Kubernetes",
-      "Amazon Elastic File System",
-      "Amazon Virtual Private Cloud",
-      "Tax",
-      "Amazon Elastic Container Service",
-      "Amazon Elastic Container Registry Public",
-      "Amazon Elastic Block Store",
-      "Amazon Elastic Compute Cloud  Compute",
-      "Amazon DynamoDB",
-      "Amazon Cognito",
-      "AmazonCloudWatch",
-      "Amazon CloudFront",
-      "AWS Key Management Service",
-      "Amazon Registrar",
-      "Amazon Route 53",
-      "Amazon Relational Database Service",
-      "Amazon Simple Storage Service",
-      "AWS Secrets Manager",
-      "Amazon Simple Notification Service",
-      "Amazon Simple Queue Service",
-    ]
-  }
+
 
 
   notification {
